2f62300d0a657a81e44b30f16196cf53cbe6b8b9
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-06-26 13:45:09 -0400

    created a moveable player that can stop at the edge of the canvas display

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const startButton = document.getElementById('startButton')
 
-const loadGame = () => {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const screenDiv = document.getElementById('screen-div');
-    screenDiv.append(canvas)
-    ctx.fillStyle = 'green'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    startButton.style.display = 'none'
-}
 
-startButton.addEventListener('click', loadGame)
+// const startButton = document.getElementById('startButton')
+
+// const loadGame = () => {
+//     const canvas = document.createElement('canvas');
+//     const ctx = canvas.getContext('2d');
+//     const screenDiv = document.getElementById('screen-div');
+//     screenDiv.append(canvas)
+//     ctx.fillStyle = 'green'
+//     ctx.fillRect(0, 0, canvas.width, canvas.height)
+//     startButton.style.display = 'none'
+// }
+
+// startButton.addEventListener('click', loadGame)
+
