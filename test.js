cd63d8d0697c4f0835e3a84287648b31667fe075
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-05 18:44:44 -0400

    fixed a bug

--- test.js
+++ test.js
@@ -1,85 +1,69 @@
 const log = console.log
-const welcomePage = () => {
-    let welcome = document.createElement('div');
-    welcome.setAttribute('id', 'welcome');
-    welcome.innerHTML = 'Welcome to The Labyrinth'
-    document.body.append(welcome)
-    let beginGame = document.createElement('button');
-    beginGame.setAttribute('id', 'startbutton')
-    beginGame.innerHTML = 'Ready to Escape?'
-    welcome.append(beginGame)
-    welcome.addEventListener('click', function () {
-        welcome.remove()
-        return newGame()
-
-
-    })
-}
-
-
-const gameEnd = (playerOutcome) => {
-    document.getElementById('labyrinthStart').remove()
-
-    let banner = document.createElement('div');
-    banner.setAttribute('id', 'gameEnd');
-    document.body.append(banner);
-    if (playerOutcome == 'winner') {
-
-        banner.innerText = 'YOU HAVE ESCAPED THE LABYRINTH!'
-    } else { banner.innerText = 'YOU HAVE FAILED TO ESCAPE!' }
-
-
-    let startAgain = document.createElement('button');
-    startAgain.setAttribute('id', 'startAgain')
-    banner.append(startAgain)
-    startAgain.innerHTML = 'Try Again?'
-
-    startAgain.addEventListener('click', function () {
-        banner.remove()
-    })
-}
+const table = console.table
 
 
 
-//create div to hold inventory, timer, and canvas
+//functions that dynamically modify my page to keep everything on one index.html
+
 newDiv = document.createElement('div')
 newDiv.setAttribute('id', 'labyrinthStart');
-//create inventory
+
 makeInventory = document.createElement('div');
 makeInventory.setAttribute('id', 'inventory');
 
-//create timer
-makeTimer = document.createElement('div');
-makeTimer.setAttribute('id', 'timer')
-
-
-//create canvas
 makeCanvas = document.createElement('canvas');
 makeCanvas.setAttribute('id', 'canvas');
 
-//create timer
-// timerLable = document.createElement('span');
-// timerLable.setAttribute('id', 'timer-label')
-// timerLable.innerText = 'TIME REMAINING:'
-// setTime = document.createElement('span');
-// setTime.setAttribute('id', 'time-left');
-// setTime.innerText = '00'
 
-//append div to body, then append inv, timer, canvas to dive
-// document.body.append(newDiv);
-// newDiv.append(makeInventory);
-// newDiv.append(makeTimer)
-// makeTimer.append(timerLable, setTime)
-// newDiv.append(makeCanvas)
+makeTimer = document.createElement('div');
+makeTimer.setAttribute('id', 'timer')
 
-//grabs html elements that we just created/ sizes canvas
+timerLable = document.createElement('span');
+timerLable.setAttribute('id', 'timer-label')
+timerLable.innerText = 'TIME REMAINING:'
+
+setTime = document.createElement('span');
+setTime.setAttribute('id', 'time-left');
+setTime.innerText = '00'
+
+
+document.body.append(newDiv);
+newDiv.append(makeInventory);
+newDiv.append(makeTimer)
+makeTimer.append(timerLable, setTime)
+newDiv.append(makeCanvas)
 const canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
-canvas.width = innerWidth + 55
-canvas.height = innerHeight - 55
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const gameEnd = (playerOutcome) => {
+    let banner = document.createElement('div');
+    banner.setAttribute('id', 'gameEnd');
+    switch (playerOutcome) {
+        case 'winner':
+            document.getElementById('labyrinthStart').remove()
+            banner.innerText = 'YOU HAVE ESCAPED THE LABYRINTH!'
+            break;
+        case 'loser':
+            document.getElementById('labyrinthStart').remove()
+            banner.innerText = 'YOU HAVE FAILED TO ESCAPE!'
+            break;
+    }
+    document.body.append(banner)
+    // let startAgain = document.createElement('button');
+    // startAgain.setAttribute('id', 'startAgain')
+    // startAgain.innerHTML = 'Try Again?'
+    // banner.append(startAgain)
+    // banner.addEventListener('click', newGame)
+}
+
+
+
+
 const inventory = document.getElementById('inventory');
 
-//this are all the classes used by game
+
 
 class Player {
     constructor() {
@@ -93,7 +77,7 @@ class Player {
         }
         this.width = 16
         this.height = 16
-        this.speed = 3.5
+        this.speed = 4
         this.doorKeys = {
             heart: false,
             club: false,
@@ -111,6 +95,7 @@ class Player {
         this.position.x += this.velocity.x
     }
 }
+
 class Key {
     constructor({ position, srcImg, keyShape }) {
         this.position = position;
@@ -139,6 +124,7 @@ class Key {
         this.draw()
     }
 }
+
 class Boundry {
     static width = 32
     static height = 32
@@ -155,6 +141,7 @@ class Boundry {
         this.draw()
     }
 }
+
 class Door {
     constructor({ position, doorName },) {
         this.position = position
@@ -184,11 +171,15 @@ class Door {
         this.draw()
     }
 }
+
 class FinishLine {
     constructor({ position }) {
         this.position = position;
         this.width = 32;
         this.height = 32;
+    }
+    gameWin() {
+
     }
     draw() {
         c.fillStyle = 'orange'
@@ -199,92 +190,14 @@ class FinishLine {
     }
 }
 
-function animate() {
-    while (gameRunning = true) {
-        requestAnimationFrame(animate)
-        c.clearRect(0, 0, canvas.width, canvas.height)
-        boundries.forEach(boundry => {
-            boundry.draw()
-            if (player.position.y + player.velocity.y <= boundry.position.y + boundry.height &&
-                player.position.y + player.velocity.y + player.height >= boundry.position.y &&
-                player.position.x + player.velocity.x + player.width >= boundry.position.x &&
-                player.position.x + player.velocity.x <= boundry.position.x + boundry.width
-            ) {
-                player.velocity.x = 0
-                player.velocity.y = 0
-            }
-        })
-        doors.forEach(door => {
-            door.draw()
-            if (player.position.y + player.velocity.y / 2 <= door.position.y + door.height &&
-                player.position.y + player.velocity.y / 2 + player.height >= door.position.y &&
-                player.position.x + player.velocity.x / 2 + player.width >= door.position.x &&
-                player.position.x + player.velocity.x / 2 <= door.position.x + door.width
-            ) {
-                if (!door.hasProperKey()) {
-                    player.velocity.x = 0
-                    player.velocity.y = 0
-                    door.doorLocked()
-                } else door.openDoor()
-            }
-        })
-
-        hiddenkeys.forEach((hiddenkey, i) => {
-            hiddenkey.draw()
-            if (player.position.y + player.velocity.y <= hiddenkey.position.y + hiddenkey.height &&
-                player.position.y + player.velocity.y + player.height >= hiddenkey.position.y &&
-                player.position.x + player.velocity.x + player.width >= hiddenkey.position.x &&
-                player.position.x + player.velocity.x <= hiddenkey.position.x + hiddenkey.width
-            ) {
-                hiddenkey.addToInventory()
-                hiddenkey.giveKey()
-                hiddenkeys.splice(i, 1)
-            }
-
-        })
-        finisharea.forEach(block => {
-            block.draw()
-            if (player.position.y + player.velocity.y <= block.position.y + block.height &&
-                player.position.y + player.velocity.y + player.height >= block.position.y &&
-                player.position.x + player.velocity.x + player.width >= block.position.x &&
-                player.position.x + player.velocity.x <= block.position.x + block.width
-            ) {
-                return gameEnd('winner')
-
-
-
-            }
-        })
-
-
-        player.update()
-        player.velocity.x = 0
-        player.velocity.y = 0
-
-
-        if (keys.right.pressed && lastKey === 'd') {
-            player.velocity.x = player.speed;
-        } else if (keys.left.pressed && lastKey === 'a') {
-            player.velocity.x = -player.speed;
-        } else if (keys.up.pressed && lastKey === 'w') {
-            player.velocity.y = -player.speed;
-        } else if (keys.down.pressed && lastKey === 's') {
-            player.velocity.y = player.speed;
-        } else {
-            player.velocity.x = 0, player.velocity.y = 0
-        }
-    }
-}
-
-//this creates the map used for the game and creates classes based on symbol found in the array of arrays in maps
 const map = [
     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', '-', '-', '-', '-', '-', '-', '-', '*', '-', '-', '-', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', '-', '-', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-',],
-    ['-', ' ', ' ', '-', 'k', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', 'k', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-',],
-    ['-', 'f', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', '-', '-', '-', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-',],
+    ['-', 'f', ' ', '-', 'k', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', 'k', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-',],
+    ['-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', '-', '-', '-', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-',],
     ['-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', '-', '-', '-', '-', ' ', ' ', '-', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-', ' ', ' ', '-',],
@@ -309,6 +222,9 @@ let padLock = 0
 let playersKeys = 0
 const lock = ['heart', 'diamond', 'club', 'spade']
 const myKeys = ['heart', 'diamond', 'spade', 'club']
+
+
+
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) => {
@@ -357,8 +273,7 @@ map.forEach((row, i) => {
     })
 })
 
-//this set the timer and runs only once the player first moves.  the functions returns true the first time its run and will not run again until the game restarts
-timeRemaining = 50
+timeRemaining = 5
 let excecuteTimer = false;
 let myTimer = (() => {
     if (!excecuteTimer) {
@@ -366,17 +281,21 @@ let myTimer = (() => {
             if (timeRemaining - 1 >= 0) {
                 timeRemaining--
                 document.getElementById('time-left').innerText = timeRemaining
+                log(timeRemaining)
             } else {
                 clearInterval(countDown)
-                document.getElementById('time-left').innerText = 'GAME OVER';
-                return gameEnd('loser');
+                document.getElementById('time-left').innerText = 'GAME OVER'
+                // document.getElementById('labyrinthStart').remove()
+                gameEnd('loser')
             }
         }, 1000)
         return excecuteTimer = true
     }
+
 })
 
 
+const player = new Player()
 const keys = {
     right: {
         pressed: false
@@ -392,11 +311,117 @@ const keys = {
     }
 }
 
-const player = new Player()
 let lastKey = ''
+
+//loops the animation frame to allow continuous changes to object states
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundries.forEach(boundry => {
+        boundry.draw()
+        if (player.position.y + player.velocity.y <= boundry.position.y + boundry.height &&
+            player.position.y + player.velocity.y + player.height >= boundry.position.y &&
+            player.position.x + player.velocity.x + player.width >= boundry.position.x &&
+            player.position.x + player.velocity.x <= boundry.position.x + boundry.width
+        ) {
+            player.velocity.x = 0
+            player.velocity.y = 0
+        }
+    })
+    doors.forEach(door => {
+        door.draw()
+        if (player.position.y + player.velocity.y <= door.position.y + door.height &&
+            player.position.y + player.velocity.y + player.height >= door.position.y &&
+            player.position.x + player.velocity.x + player.width >= door.position.x &&
+            player.position.x + player.velocity.x <= door.position.x + door.width
+        ) {
+            if (!door.hasProperKey()) {
+                player.velocity.x = 0
+                player.velocity.y = 0
+                door.doorLocked()
+            } else door.openDoor()
+        }
+    })
+
+    hiddenkeys.forEach((hiddenkey, i) => {
+        hiddenkey.draw()
+        if (player.position.y + player.velocity.y <= hiddenkey.position.y + hiddenkey.height &&
+            player.position.y + player.velocity.y + player.height >= hiddenkey.position.y &&
+            player.position.x + player.velocity.x + player.width >= hiddenkey.position.x &&
+            player.position.x + player.velocity.x <= hiddenkey.position.x + hiddenkey.width
+        ) {
+            hiddenkey.addToInventory()
+            hiddenkey.giveKey()
+            hiddenkeys.splice(i, 1)
+        }
+
+    })
+    finisharea.forEach(block => {
+        block.draw()
+        if (player.position.y + player.velocity.y <= block.position.y + block.height &&
+            player.position.y + player.velocity.y + player.height >= block.position.y &&
+            player.position.x + player.velocity.x + player.width >= block.position.x &&
+            player.position.x + player.velocity.x <= block.position.x + block.width
+        ) {
+            //you win function
+            gameEnd('winner')
+            // timeRemaining = 00
+            // document.getElementById('time-left').innerText = 'YOU WIN'
+            // document.getElementById('labyrinthStart')
+
+            // gameEnd('winner')
+
+
+        }
+    })
+
+
+    player.update()
+    player.velocity.x = 0
+    player.velocity.y = 0
+
+
+    if (keys.right.pressed && lastKey === 'd') {
+        player.velocity.x = player.speed;
+    } else if (keys.left.pressed && lastKey === 'a') {
+        player.velocity.x = -player.speed;
+    } else if (keys.up.pressed && lastKey === 'w') {
+        player.velocity.y = -player.speed;
+    } else if (keys.down.pressed && lastKey === 's') {
+        player.velocity.y = player.speed;
+    } else {
+        player.velocity.x = 0, player.velocity.y = 0
+    }
+
+}
 
 animate()
 
+// const StartPage = () => {
+//     let welcome = document.createElement('div');
+//     welcome.setAttribute('id', 'welcome');
+//     welcome.innerHTML = 'Welcome to The Labyrinth'
+//     document.body.append(welcome)
+//     let beginGame = document.createElement('button');
+//     beginGame.setAttribute('id', 'startbutton')
+//     beginGame.innerHTML = 'Ready to Escape?'
+//     welcome.append(beginGame)
+//     welcome.addEventListener('click', log('clicked'))
+
+// }
+
+
+//this event listener will log true if you click on the player entity within canvas
+addEventListener('click', (event) => {
+    let mouseX = event.x
+    let mouseY = event.y
+    if (mouseX >= player.position.x && mouseX <= player.position.x + player.width &&
+        mouseY >= player.position.y && mouseY <= player.position.y + player.height)
+        console.log('true')
+    else console.log('false')
+})
+
+//event listeners for keyboard
 addEventListener('keydown', ({ key }) => {
     myTimer()
     switch (key) {
@@ -434,5 +459,3 @@ addEventListener('keyup', ({ key }) => {
             break;
     }
 })
-
-
